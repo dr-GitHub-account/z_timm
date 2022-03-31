1dfbc46ebfa38f4fb519ec2432ef7a22f24375fa
xiongdengrui <1150119424@qq.com>
2022-03-31 17:15:39 +0800

    test

---
+++ getting_started.py
@@ -0,0 +1,9 @@
+import timm
+
+# m = timm.create_model('mobilenetv3_large_100', pretrained=True)
+# m.eval()
+
+# print(len(timm.list_models('*')))
+# print(len(timm.list_models(pretrained = True)))
+
+model = timm.create_model('resnet50d', pretrained = True)
